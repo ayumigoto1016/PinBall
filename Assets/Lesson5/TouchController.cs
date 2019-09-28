86486979c9b46930428303b61055905263f2589d
ayumi goto <ayumi.goto.1016@gmail.com>
2019-09-28 22:53:10 +0900

    発展課題

---
+++ Assets/Lesson5/TouchController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	private HingeJoint myHingeJoint;
+	private float defaultAngle = 20;
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		this.myHingeJoint = GetComponent<HingeJoint> ();
+		SetAngle (this.defaultAngle);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch (0);
+
+			if (touch.position.x > Screen.width * 0.5f && tag == "RightFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (touch.position.x < Screen.width * 0.5f && tag == "LeftFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (touch.position.x < Screen.width * 0.5f && tag == "RightFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+			if (touch.position.x > Screen.width * 0.5f && tag == "LeftFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+
+			/*if(touch.position.x > Screen.width * 0.5f && tag =="RightFripperTag"){
+				
+				SetAngle (this.flickAngle);
+
+			}else{
+
+				SetAngle (this.defaultAngle);
+
+			}
+
+			if (touch.position.x < Screen.width * 0.5f && tag == "LeftFripperTag") {
+
+				SetAngle (this.flickAngle);
+
+			}
+			else{
+
+				SetAngle (this.defaultAngle);
+			    
+			}*/
+
+		} else {
+
+			SetAngle (this.defaultAngle);
+
+		}
+
+	}
+
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+
+	}
+}
