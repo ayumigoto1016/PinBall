676df94feae6112424d779c74a48284fbe379e31
ayumi goto <ayumi.goto.1016@gmail.com>
2019-09-28 16:20:00 +0900

    Lesson5 スコア表示
    GameObjectに衝突時のスコア表示をしました。

---
+++ Assets/Lesson5/ScoreController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class ScoreController : MonoBehaviour {
+	
+	public Text scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		score = 0;
+		SetScore ();
+
+
+	}
+
+
+	void Update (){
+		
+
+
+	}
+
+
+	void OnCollisionEnter(Collision collision)
+    {
+		string tag = collision.gameObject.tag;
+
+		if (tag == "SmallStarTag") { 
+			score += 3;
+		} else if (tag == "LargeStarTag") {
+			score += 6;
+		} else if (tag == "SmallCloudTag") {
+			score += 9;
+		} else if (tag == "LargeCloudTag") {
+			score += 12;
+		}
+
+		SetScore ();
+
+
+	}
+
+	void SetScore(){
+		scoreText.text = string.Format("Score:{0}", score);
+	}
+
+
+
+}
+	
